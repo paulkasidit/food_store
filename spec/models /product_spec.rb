3d6849eb4e9f2a432fc89b22ed9bbfdb1f510c4e
Paul Kasidit <paulkasidit@gmail.com>
2022-10-21 01:33:35 -0700

    test for foreign key  relation products and review

--- spec/models /product_spec.rb
+++ spec/models /product_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Product do
   # Tests will go here.
+  it { should have_many(:reviews) }
 end
--- spec/models /review_spec.rb
+++ spec/models /review_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Review do
   # Tests will go here.
+  it { should belong_to(:product) }
 end
