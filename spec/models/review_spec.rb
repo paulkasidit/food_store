aa06e0c6cf59d9095c84b53ea5e2550335d78cae
Paul Kasidit <paulkasidit@gmail.com>
2022-10-25 19:41:39 -0700

    work on validation testing for reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 describe Review do
   # Tests will go here.
   it { should belong_to(:product) }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :content_body }
+  it { should validate_presence_of :rating }
 end
