afb8b59ec307e0e5fee7c19a02e308e909a3a86e
Paul Kasidit <paulkasidit@gmail.com>
2022-10-25 19:31:46 -0700

    writing validation tests for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 describe Product do
   # Tests will go here.
   it { should have_many(:reviews) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_of_origin }
 end
