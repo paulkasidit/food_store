4571ad316927b309ab45b32201423358cd16fec5
Paul Kasidit <paulkasidit@gmail.com>
2022-10-27 01:50:40 -0700

    seeded database with fake coffee products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movies.first
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Coffee.blend_name,
+                        cost: 10,
+                        country_of_origin: Faker::Coffee.origin )
+end
+
+p "Created #{Product.count} products"
