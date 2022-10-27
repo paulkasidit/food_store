56d409fa69363229130b4f0b2a2ee682f60ec8fb
Paul Kasidit <paulkasidit@gmail.com>
2022-10-27 15:16:12 -0700

    finish seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,30 @@ require 'faker'
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first
 
+product_reviews = ['Good stuff, I love the fragrant smell. It is out of this world.',
+  'Ok quality, I hate the fragrant smell. It is not out of this world. Look elsewhere',
+  'Satisfactory product, I can stand the rancid smell. It is better to look at other products. Look elsewhere',
+  'This excellent product is extremely satisfying to the palate. Good for my discerning tastes.']
+
+product_ratings = [1,2,3,4,5]
+
 Product.destroy_all
 
 50.times do |index|
   Product.create!(name: Faker::Coffee.blend_name,
-                        cost: 10,
-                        country_of_origin: Faker::Coffee.origin )
-end
+                  cost: 10,
+                  country_of_origin: Faker::Coffee.origin )
+  end
 
 p "Created #{Product.count} products"
+
+Review.destroy_all 
+
+250.times do |index| 
+  Review.create!(author: Faker::FunnyName.name, 
+                content_body: (product_reviews).sample, 
+                rating: (product_ratings).sample,
+                product_id: (Product.ids).sample)
+  end
+
+p "Created #{Review.count} reviews"
