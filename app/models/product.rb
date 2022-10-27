5b1ec8a67819df9d2f7a8d67b0b4b22e8675ae33
Paul Kasidit <paulkasidit@gmail.com>
2022-10-27 01:36:06 -0700

    finished titelization methods for product database

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,20 @@
 class Product < ApplicationRecord
+
+  before_save(:titleize_name, :titleize_country_of_origin)
+
   has_many :reviews, dependent: :destroy 
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
+
+  private
+
+    def titleize_name
+      self.name = self.name.titleize
+    end
+
+    def titleize_country_of_origin
+      self.country_of_origin = self.country_of_origin.titleize
+    end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,15 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
+
+  it("titleizes the name of the product") do
+    product = Product.create({name: "french brie", cost: 5.99, country_of_origin: "france"})
+    expect(product.name()).to(eq("French Brie"))
+  end
+
+  it("titleizes the name of country of origin") do
+    product = Product.create({name: "french brie", cost: 5.99, country_of_origin: "france"})
+    expect(product.country_of_origin()).to(eq("France"))
+  end
+
 end
